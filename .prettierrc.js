e7f3fdccd9e0d22d07cd1b4bd1e8d79c05a319dc
Andre Rocco <andre.amaral.rocco@gmail.com>
2022-12-21 11:01:47 -0300

    Instalados e configurados ESLint e Prettier

---
+++ .prettierrc.js
@@ -0,0 +1,26 @@
+/* module.exports = {
+    semi: true,
+    trailingComma: 'all',
+    singleQuote: true,
+    printWidth: 120,
+    tabWidth: 4
+} */
+module.exports = {
+    arrowParens: 'always',
+    bracketSpacing: true,
+    endOfLine: 'crlf',
+    htmlWhitespaceSensitivity: 'ignore',
+    insertPragma: false,
+    jsxSingleQuote: false,
+    printWidth: 80,
+    proseWrap: 'always',
+    quoteProps: 'as-needed',
+    requirePragma: false,
+    semi: true,
+    singleQuote: true,
+    tabWidth: 4,
+    trailingComma: 'all',
+    useTabs: false,
+    vueIndentScriptAndStyle: false,
+    embeddedLanguageFormatting: 'off',
+};
