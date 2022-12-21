41d51f6e6cc513f89c9218f3d9ddfcf675c4ef44
Andre Rocco <andre.amaral.rocco@gmail.com>
2022-12-21 16:07:54 -0300

    Criado componente de botão

---
+++ src/components/Button/index.jsx
@@ -0,0 +1,17 @@
+import './styles.css';
+import P from 'prop-types';
+
+export const Button = ({ label, type, onPress, style }) => {
+	return (
+		<button className="button" type={type} style={style} onClick={onPress}>
+			{label}
+		</button>
+	);
+};
+
+Button.propTypes = {
+	label: P.string.isRequired,
+	onPress: P.func.isRequired,
+	style: P.object,
+	type: P.oneOf(['button', 'submit', 'reset']),
+};
