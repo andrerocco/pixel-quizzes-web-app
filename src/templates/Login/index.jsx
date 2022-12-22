d42f0f4a68ed08b39855984ae8341f4adf6ec574
Andre Rocco <andre.amaral.rocco@gmail.com>
2022-12-22 01:53:59 -0300

    Iniciada criação da página de login

--- src/templates/Login/index.jsx
+++ src/templates/Login/index.jsx
@@ -1,7 +1,37 @@
 import './styles.css';
+import { useState } from 'react';
+// Componentes
+import { Button } from '../../components/Button';
+import { TextInput } from '../../components/TextInput';
+import { PasswordInput } from '../../components/PasswordInput';
 
 function Login() {
-	return <div className="Login">Login</div>;
+    /* eslint-disable no-unused-vars */
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = (event) => {
+        console.log('Email: ' + email + ' Password: ' + password);
+
+        event.preventDefault();
+    };
+
+    return (
+        <div className="Login">
+            <form onSubmit={(e) => handleLogin(e)}>
+                <TextInput
+                    type="email"
+                    id="email-input"
+                    placeholder="Email"
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                />
+                <PasswordInput placeholder="Password" onChange={(e) => setPassword(e.target.value)} required />
+                <Button type="submit" label="Login" />
+            </form>
+        </div>
+    );
 }
 
 export default Login;
