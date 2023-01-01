61e6f33760808db3a7186702aff31f191b75101b
Andre Rocco <andre.amaral.rocco@gmail.com>
2023-01-01 17:46:23 -0300

    Criadas funções de fetch das APIs

--- src/index.jsx
+++ src/index.jsx
@@ -9,10 +9,7 @@ import { routes } from './routes';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
-        {/* Envolver a aplicação por inteira com o AuthProvider permite o acesso ao contexto de autenticação por toda a aplicação */}
-        <AuthProvider>
-            <RouterProvider router={routes} />
-        </AuthProvider>
-    </React.StrictMode>,
+    <AuthProvider>
+        <RouterProvider router={routes} />
+    </AuthProvider>,
 );
--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,13 +1,42 @@
-import { SearchField } from '../../components/SearchField';
 import './styles.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+// Components
+import { SearchField } from '../../components/SearchField';
 
 function Home() {
+    /* eslint-disable no-unused-vars */
+
+    const [user, setUser] = useState({});
+    const [quizzes, setQuizzes] = useState([]);
+
+    function fetchUser() {
+        return axios.get('https://my-json-server.typicode.com/higorpo/trilha-dev-json-server/profile');
+    }
+
+    function fetchAllQuizzes() {
+        return axios.get('https://my-json-server.typicode.com/higorpo/trilha-dev-json-server/quizzes');
+    }
+
+    useEffect(() => {
+        (async () => {
+            await fetchUser().then((response) => {
+                setUser(response.data);
+            });
+        })(); // Fetches the user data and sets it to the user state
+        (async () => {
+            await fetchAllQuizzes().then((response) => {
+                setQuizzes(response.data);
+            });
+        })(); // Fetches all posts and sets it to the posts state
+    }, []);
+
     return (
         <div className="HomeWrapper">
             <div className="home-header">
                 <div id="home-header-left">
                     <p>Olá,</p>
-                    <h1>Juliana Antonieta</h1>
+                    <h1>userName</h1>
                 </div>
                 <div id="home-header-right">
                     <button type="button">Histórico</button>
