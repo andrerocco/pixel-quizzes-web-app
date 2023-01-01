80fa8a94ea4b1d2f64f87e1b122886b097783a0e
Andre Rocco <andre.amaral.rocco@gmail.com>
2023-01-01 14:58:01 -0300

    Implementado header na página 'Home'

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,7 +1,26 @@
+import { SearchField } from '../../components/SearchField';
 import './styles.css';
 
 function Home() {
-    return <div className="Home">Home</div>;
+    return (
+        <div className="HomeWrapper">
+            <div className="home-header">
+                <div id="home-header-left">
+                    <p>Olá,</p>
+                    <h1>Juliana Antonieta</h1>
+                </div>
+                <div id="home-header-right">
+                    <button type="button">Histórico</button>
+                    <button type="button">Temas</button>
+                    <SearchField
+                        placeholder="Pesquisar quiz"
+                        style={{ width: '404px' }}
+                        onChange={(e) => console.log(e.target.value)}
+                    />
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Home;
