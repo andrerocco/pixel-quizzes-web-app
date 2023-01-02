a25f1cbdc797b14d5ad60fb5a9de16d34ff0bdc4
Andre Rocco <andre.amaral.rocco@gmail.com>
2023-01-01 22:43:04 -0300

    Criado componente de grade dos cartões

---
+++ src/components/QuizGrid/index.jsx
@@ -0,0 +1,26 @@
+import './styles.css';
+import P from 'prop-types';
+// Components
+import { QuizCard } from '../QuizCard';
+
+export const QuizGrid = ({ quizzes }) => {
+    return (
+        <div className="QuizGridWrapper">
+            {quizzes.map((quiz) => {
+                return (
+                    <QuizCard
+                        key={quiz.id}
+                        cardImage={quiz.banner_image}
+                        difficulty={quiz.difficulty}
+                        title={quiz.title}
+                        description={quiz.short_description}
+                    />
+                );
+            })}
+        </div>
+    );
+};
+
+QuizGrid.propTypes = {
+    quizzes: P.array,
+};
