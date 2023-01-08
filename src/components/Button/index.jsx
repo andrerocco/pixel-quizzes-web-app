aef3e4f2bd9f51ab174aba4d51f467263f9afb85
Andre Rocco <andre.amaral.rocco@gmail.com>
2023-01-08 18:49:53 -0300

    Correção da width do componente

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,10 +1,10 @@
 import './styles.css';
 import P from 'prop-types';
 
-export const Button = ({ label, type, onPress, style }) => {
+export const Button = ({ label, type = 'button', onClick, style }) => {
     return (
         <div className="ButtonWrapper">
-            <button className="button" type={type} style={style} onClick={onPress}>
+            <button className="button" type={type} style={style} onClick={onClick}>
                 {label}
             </button>
         </div>
@@ -13,7 +13,7 @@ export const Button = ({ label, type, onPress, style }) => {
 
 Button.propTypes = {
     label: P.string.isRequired,
-    onPress: P.func,
+    onClick: P.func,
     style: P.object,
     type: P.oneOf(['button', 'submit', 'reset']),
 };
